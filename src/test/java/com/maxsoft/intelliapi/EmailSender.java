a8bde32f8850e87677c4e8b6b893fddd23f58943
Osanda Deshan <osanda.deshan@gmail.com>
2018-06-27 19:15:41 +0530

    Added EmailSender.java

---
+++ src/test/java/com/maxsoft/intelliapi/EmailSender.java
@@ -0,0 +1,19 @@
+package com.maxsoft.intelliapi;
+
+import com.maxsoft.intelliapi.util.Email;
+import com.maxsoft.intelliapi.util.JsonReportReader;
+
+
+/**
+ * Created by Osanda on 3/31/2018.
+ */
+
+
+public class EmailSender {
+
+    public static void main(String[] args) {
+        Email.send(JsonReportReader.getExecutionResults());
+    }
+
+
+}
