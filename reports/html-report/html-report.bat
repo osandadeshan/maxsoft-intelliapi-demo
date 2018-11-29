eb3e986e24f5d10541d0890951536e5fbea4cecf
Osanda Deshan <osanda.deshan@gmail.com>
2018-11-29 10:37:40 +0530

    Conflicts resolving

--- reports/html-report/html-report.bat
+++
@@ -1,2 +0,0 @@
-@echo off 
-C:\Users\UnimaOs\AppData\Roaming\gauge\plugins\html-report\4.0.3\bin\html-report.exe %*
--- reports/html-report/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var index = {"Tags":{"get_pi_token":["specs\\Access Token\\Get Production PI Token.html"],"production":["specs\\Access Token\\Get Production PI Token.html"]},"Specs":{"Get Production PI Token Specification":["specs\\Access Token\\Get Production PI Token.html"]}};
