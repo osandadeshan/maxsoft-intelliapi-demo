de189c332a320b49a12b95135f65aa7d631bf4ef
Osanda Deshan Nimalarathna <osanda.deshan@gmail.com>
2018-12-14 11:54:30 +0530

    Update QA BVT.bat

--- bat_files/test_runner_bats/QA BVT.bat
+++ bat_files/test_runner_bats/QA BVT.bat
@@ -4,8 +4,8 @@ echo Executing MaxSoft IntelliAPI QA BVT API Test Automation Suite.............
 cd ..
 cd .. 
 
-call mvn gauge:execute -DspecsDir="specs\Pre Test" -Denv="QA"
-call mvn gauge:execute -DspecsDir="specs\Data Stores,specs\Other" -Denv="QA"
+call mvn test-compile gauge:execute -DspecsDir="specs\Pre Test" -Denv="QA"
+call mvn test-compile gauge:execute -DspecsDir="specs\Data Stores,specs\Other" -Denv="QA"
 
 call mvn clean -DemailConfigEnv=QA install exec:java
 
